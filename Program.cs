ccf56150bdd15a1d43efb74ec5807140a95932b6
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-10-23 23:01:43 -0300

    Desafio Concluido.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456", "Nokia 2660 Flip", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("789012", "Iphone 14", "222222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
